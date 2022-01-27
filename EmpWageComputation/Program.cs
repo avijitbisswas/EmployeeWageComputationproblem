d6f12cc525e20becb1f5cc96ae644c1ddd64a80a
avijit <avijit02biswas@gmail.com>
2022-01-27 14:18:01 +0530

    [Avijit] DailyWage

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -10,23 +10,31 @@ namespace EmpWageComputation
     {
         public static void Main()
         {
-            const int EMP_PRESENT = 0, EMP_ABSENT = 1;
+            const int EMP_PRESENT = 0, EMP_ABSENT = 1, PAY_PER_HOUR = 20, FULL_WORK_HOUR = 8;
+            Console.WriteLine("Employement Wage Computation Program");
+            Console.ReadLine();
             Random random = new Random();
             int empcheck = random.Next(0, 2);
-            if (empcheck == 0)
+            if (empcheck == EMP_PRESENT) 
             {
-                Console.WriteLine("Employee is Preent");
+                Console.WriteLine("Employee is present");
+                Console.WriteLine("Daily Wage of the employee is " +PAY_PER_HOUR*FULL_WORK_HOUR);
                 Console.ReadLine();
             }
-            else
+            else 
             {
-                Console.WriteLine("Employe is absent");
+                Console.WriteLine("Employee is absent");
                 Console.ReadLine();
-                
             }
-            return;
+
+        
            
+
+
+
+
+
         }
-                
+
     }
 }
