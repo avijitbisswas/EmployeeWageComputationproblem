e1827bb1db11f8332ab9c8407ca527abb5352c45
avijit <avijit02biswas@gmail.com>
2022-01-27 12:46:49 +0530

    [AVijit]EmpWageComputationCreated

---
+++ EmpWageComputation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("EMPLOYEE WAGE COMPUTATION");
+            Console.ReadLine();
+        }
+    }
+}
