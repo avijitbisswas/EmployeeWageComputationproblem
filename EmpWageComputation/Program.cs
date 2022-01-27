1e0b2986e730d59c17dd6c2445e6a72d3c6277f9
avijit <avijit02biswas@gmail.com>
2022-01-27 14:41:30 +0530

    [Avijit] PartTimeUsingIF

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -10,16 +10,27 @@ namespace EmpWageComputation
     {
         public static void Main()
         {
-            const int EMP_PRESENT = 0, EMP_ABSENT = 1, PAY_PER_HOUR = 20, FULL_WORK_HOUR = 8;
+            const int EMP_PRESENT = 0, EMP_ABSENT = 1, PAY_PER_HOUR = 20, FULL_WORK_HOUR = 8 , PART_TIME_WORK=4;
             Console.WriteLine("Employement Wage Computation Program");
             Console.ReadLine();
             Random random = new Random();
-            int empcheck = random.Next(0, 2);
-            if (empcheck == EMP_PRESENT) 
+            int empcheck = random.Next(0,2);
+            if (empcheck == 0) 
             {
-                Console.WriteLine("Employee is present");
-                Console.WriteLine("Daily Wage of the employee is " +PAY_PER_HOUR*FULL_WORK_HOUR);
-                Console.ReadLine();
+                Random random2 = new Random();
+                int work = random2.Next(0, 2);
+                if (work==0) //full day work taking random as 0
+                {
+
+                    Console.WriteLine("Employee is present");
+                    Console.WriteLine("Daily Wage of the employee is " + PAY_PER_HOUR * FULL_WORK_HOUR);
+                    Console.ReadLine();
+                }
+                else 
+                {
+                    Console.WriteLine("Employee is present half day");
+                    Console.WriteLine("Part Time Wage of Employee is" +PAY_PER_HOUR*PART_TIME_WORK);
+                }
             }
             else 
             {
